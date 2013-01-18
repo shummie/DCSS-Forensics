00ed0353b68451bf9d08f4fc14e56d6c3cca4511
Shummie <rbshum@gmail.com>
2013-01-17 22:35:33 -0600

    Extracting version from header

--- gameinfo.py
+++ gameinfo.py
@@ -9,7 +9,10 @@ class gameInfo:
     # infomation found in the header
     # examples: 0.10, 0.11, 0.12a
     # will need to truncate from full version information
-    version = ""
+    # in two formats: versionLong and versionShort
+    # versionLong contains the full version information
+    # versionShort keeps it at the first 6 characters
+
 
     rawData = []
     header = []
@@ -44,5 +47,13 @@ class gameInfo:
     ## This function extracts the data from the data element fields.
     ## It will populate data only if the data element field is not empty.
         
-    def extractData():
-        
+    def extractData(self):
+
+        ## Check the header
+        if len(self.header) != 0: self.extractHeader()
+
+    ## header information stored in the following format:
+    ##  Dungeon Crawl Stone Soup version 0.12-a0-1616-ge3ef79a (tiles) character file.
+    def extractHeader(self):
+        self.versionLong = self.header[0].split()[5]
+        self.versionShort = self.versionLong[0:6]
