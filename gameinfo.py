f9ed4bb6f9eb1f27ea3d92d6dc96e1fec96fe70b
Shummie <rbshum@gmail.com>
2013-01-17 22:57:55 -0600

    score, name, and title extracted from hiscore

--- gameinfo.py
+++ gameinfo.py
@@ -49,11 +49,42 @@ class gameInfo:
         
     def extractData(self):
 
-        ## Check the header
         if len(self.header) != 0: self.extractHeader()
+        if len(self.hiscore) != 0: self.extractHiscore()
+        
 
-    ## header information stored in the following format:
+    ## header information stored in the following format
     ##  Dungeon Crawl Stone Soup version 0.12-a0-1616-ge3ef79a (tiles) character file.
     def extractHeader(self):
         self.versionLong = self.header[0].split()[5]
         self.versionShort = self.versionLong[0:6]
+        # Note: future add - webtile/tile/console version can also be extracted
+
+    ## hiscore information stored in the following format
+    ## 51752 Ray the Unseen (level 14, -2/92 HPs)
+    ##         Began as a Kobold Berserker on Jan 16, 2013.
+    ##         Was the Champion of Trog.
+    ##         Killed by divine providence
+    ##         ... invoked by an orc priest
+    ##          (14 damage)
+    ##         ... on Level 1 of the Orcish Mines on Jan 17, 2013.
+    ##         The game lasted 01:28:44 (24313 turns).
+    def extractHiscore(self):
+        self.score = self.hiscore[0].split()[0]
+
+        # name may have spaces in it. need to search until 'the' is found
+        lineIndex = 1
+        lineSplit = self.hiscore[0].split()
+        self.name = lineSplit[lineIndex]
+        lineIndex += 1
+        while lineSplit[lineIndex] != "the":
+            self.name += " " + lineSplit[lineIndex]
+            lineIndex += 1
+
+        lineIndex += 1
+        self.title = lineSplit[lineIndex]
+        lineIndex += 1
+        while lineSplit[lineIndex] != "(level":
+            self.title += " " + lineSplit[lineIndex]
+            lineIndex += 1
+        
