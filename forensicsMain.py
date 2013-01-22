8c815b07cad09797d6e6c5ffcc779e8c0583be70
shummie <rbshum@gmail.com>
2013-01-22 10:02:47 -0600

    First cut at running main
    Set up starting main file. Currently reads the records. Next step is to
    output into csv to validate information.

---
+++ forensicsMain.py
@@ -0,0 +1,33 @@
+#   Dungeon Crawl Stone Soup Forensics
+#   Copyright (C) 2013  Raymond Shum
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import forensicsParser
+import os
+import glob
+
+## Enter path below to main DCSS directory
+PATH = "C:/Users/shumr/Documents/Ray/stone_soup-tiles-0.11"
+OUTFILE = "test.csv"
+
+os.chdir(PATH + "/morgue")
+
+gameCollection = []
+
+for files in glob.glob("*.txt"):
+    gameCollection.append(forensicsParser.readGameRecord(files))
+
+
+    
