9222cf7d74e3a753473622d1dffe8b8321f50309
shummie <rbshum@gmail.com>
2013-02-28 10:13:12 -0600

    v0.04

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ import sys
 from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"silent": ["s"], "create_shared_zip" : False, "include_files":["config.ini", "style.css", "LICENSE", "README.md", "Version History"]}
+build_exe_options = {"silent": ["s"], "create_shared_zip" : False, 
+                     "include_files":["config.ini", "style.css", "LICENSE", "README.md", "Version History"]}
 #buildOptions = dict(create_shared_zip = False, silent = ["s"])
 
 # GUI applications require a different base on Windows (the default is for a
@@ -12,7 +13,7 @@ if sys.platform == "win32":
     base = "Win32GUI"
 
 setup(  name = "DCSSForensics",
-        version = "0.03",
+        version = "0.04",
         description = "DCSS Forensics",
         options = {"build_exe": build_exe_options},
         #options = dict(build_exe = buildOptions),
@@ -22,3 +23,4 @@ setup(  name = "DCSSForensics",
 # To create executable, run the following on the command prompt in the main directory
 # python setup.py build
 # Cx_Freeze needs to be installed. Also, the PATH variables may need to be modified.
+# Personal note: "C:\program files (x86)\python\python" setup.py build
